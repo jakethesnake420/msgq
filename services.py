919406fe2775f7654668543e51ffad26c72caa61
jakethesnake420 <fastermaster12@gmail.com>
2024-01-16 23:08:36 -0600

    increase micd rate to 12.5(80ms samples)

--- services.py
+++ services.py
@@ -80,8 +80,8 @@ services: dict[str, tuple] = {
   "uiPlan": (True, 20., 40.),
   "qRoadEncodeIdx": (False, 20.),
   "userFlag": (True, 0., 1),
-  "microphone": (True, 10., 10),
-  "microphoneRaw": (False, 0., 0.),
+  "microphone": (True, 12.5, 10),
+  "microphoneRaw": (False, 12.5., 0.),
   "speechToText": (False, 0., 0.),
 
   # debug
